eac67771210ecbf701c5522ffe85c7a152bbc21a
Yaroslav Melnychuk <melnichukjarik@gmail.com>
2018-09-05 22:39:05 +0300

    Show message about authorisation results

--- src/components/LoginUserForm/index.js
+++ src/components/LoginUserForm/index.js
@@ -5,7 +5,8 @@ import { loginUser } from '../../actions';
 
 class loginUserForm extends Component {
   state = {
-    userName: ''
+    userName: '',
+    showMessage: false
   }
 
   inputChangeHandler = event => {
@@ -15,20 +16,37 @@ class loginUserForm extends Component {
   loginUserHandler = event => {
     event.preventDefault();
     this.props.loginUser(this.state.userName);
+    this.setState({ showMessage: true });
   }
 
   render() {
+    const { isUserLoggedIn, userNickname } = this.props;
+
     return (
-      <form className="login-user" onSubmit={event => this.loginUserHandler(event)}>
-        <input type="text" onChange={event => this.inputChangeHandler(event)} />
-        <input type="submit" value="Login" disabled={this.state.userName.length === 0} />
-      </form>
+      <div className="login-user">
+        <form className="login-user__form" onSubmit={event => this.loginUserHandler(event)}>
+          <input type="text" onChange={event => this.inputChangeHandler(event)} />
+          <input type="submit" value="Login" disabled={this.state.userName.length === 0} />
+        </form>
+        {
+          (this.state.showMessage) ?
+            (isUserLoggedIn) ?
+              <div className="msg msg_success">User {userNickname} was successfully logged in</div> :
+              <div className="msg msg_error">Login failed</div>
+            : null
+        }
+      </div>
     )
   }
 };
 
+const mapStateToProps = state => ({
+  isUserLoggedIn: state.userReducer.currentUser.isLoggedIn,
+  userNickname: state.userReducer.currentUser.nickname
+});
+
 const mapDispatchToProps = dispatch => ({
   loginUser: userName => dispatch(loginUser(userName))
-})
+});
 
-export default connect(null, mapDispatchToProps)(loginUserForm);
+export default connect(mapStateToProps, mapDispatchToProps)(loginUserForm);
