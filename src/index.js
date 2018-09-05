628aef3ef4c5a9c36e5ee1f34db62ea8775582e2
Yaroslav Melnychuk <melnichukjarik@gmail.com>
2018-09-05 13:14:01 +0300

    Connect Redux devtools to project

--- src/index.js
+++ src/index.js
@@ -16,7 +16,10 @@ const rootReducer = combineReducers({
   userReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
