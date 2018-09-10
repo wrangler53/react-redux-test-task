c46ba028f76fe081ef62366a78cce8d19dcdcdfc
Yaroslav Melnychuk <melnichukjarik@gmail.com>
2018-09-10 20:40:04 +0300

    Show errors connected with comment leave in Comments component

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,25 +14,15 @@ export const registerUser = userName => ({
 export const authUser = userName => (dispatch, getStore) => {
   const users = getStore().userReducer.users;
 
-  if (userName.length === 0) {
-    throw new Error('User name length should be greather than 0');
+  if (checkIsUserExistsInDB(users, userName)) {
+    dispatch(loginUser(userName));
   } else {
-    if (checkIsUserExistsInDB(users, userName)) {
-      dispatch(loginUser(userName));
-    } else {
-      dispatch(registerUser(userName));
-      dispatch(loginUser(userName, true));
-    }
+    dispatch(registerUser(userName));
+    dispatch(loginUser(userName, true));
   }
 }
 
-export const addComment = (newsItemId, commentText, author) => {
-  if ((commentText.length === 0 || commentText.length > 100) || author == null) {
-    throw new Error('User is not logged in in system');
-  } else {
-    return {
-      type: actionsTypes.ADD_COMMENT,
-      payload: { id: new Date(), newsItemId, commentText, author }
-    }
-  }
-}
+export const addComment = (newsItemId, commentText, author) => ({
+  type: actionsTypes.ADD_COMMENT,
+  payload: { id: new Date(), newsItemId, commentText, author }
+});
--- src/containers/Comments/index.js
+++ src/containers/Comments/index.js
@@ -9,7 +9,10 @@ import CommentItem from '../../components/CommentItem';
 class Comments extends Component {
   state = {
     comment: '',
-    maxSymbolsCount: 100
+    maxSymbolsCount: 100,
+    showMaxSymbolsError: false,
+    showCommentErrorMessage: false,
+    showUserErrorMessage: false
   }
 
   inputChangeHandler = event => {
@@ -18,13 +21,36 @@ class Comments extends Component {
 
   addCommentHandler = event => {
     event.preventDefault();
-    this.props.addComment(this.props.newsItemId, this.state.comment, this.props.userNickname);
-    this.setState({ comment: '' });
+    const { newsItemId, userNickname } = this.props;
+    const { comment, maxSymbolsCount } = this.state;
+
+    if (userNickname === null) {
+      this.setState({ showUserErrorMessage: true });
+    } else if (comment.length === 0) {
+      this.setState({ showCommentErrorMessage: true });
+    } else if (comment.length > maxSymbolsCount) {
+      this.setState({ showMaxSymbolsError: true });
+    } else {
+      this.props.addComment(newsItemId, comment, userNickname);
+      this.setState({
+        comment: '',
+        showMaxSymbolsError: false,
+        showCommentErrorMessage: false,
+        showUserErrorMessage: false
+      });
+    }
   }
 
   render() {
     const { commentsList, isUserLoggedIn } = this.props;
-    const { comment, maxSymbolsCount } = this.state;
+    const {
+      comment,
+      maxSymbolsCount,
+      showMaxSymbolsError,
+      showCommentErrorMessage,
+      showUserErrorMessage
+    } = this.state;
+
     return (
       <div className="comments">
         <form className="leave-comment" onSubmit={event => this.addCommentHandler(event)}>
@@ -35,8 +61,25 @@ class Comments extends Component {
             maxLength={maxSymbolsCount}
             onChange={event => this.inputChangeHandler(event)}
           />
-          <div className="characters-count">
-            Characters left: {maxSymbolsCount - comment.length}
+          <div className="msg-container">
+            <div className="characters-count">
+              Characters left: {maxSymbolsCount - comment.length}
+            </div>
+            {
+              (showCommentErrorMessage) ?
+                <div className="msg msg_error">Comments field shouldn`t be empty</div> :
+                null
+            }
+            {
+              (!isUserLoggedIn && showUserErrorMessage) ?
+                <div className="msg msg_error">Register or log in please to leave a comment!</div> :
+                null
+            }
+            {
+              (showMaxSymbolsError) ?
+                <div className="msg msg_error">Commit length should be less than {maxSymbolsCount} symbols</div> :
+                null
+            }
           </div>
           <input
             type="submit"
